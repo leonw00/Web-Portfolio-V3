67148518f9e185f8b6b2f39aaa61532aac0f0714
leo <leonardow1908@gmail.com>
2022-03-02 22:20:51 -0600

    temp rm photo

--- src/components/pages/HeroSection.js
+++ src/components/pages/HeroSection.js
@@ -21,7 +21,7 @@ function HeroSection() {
                     {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" className = "hero-blob">
                         <path fill="#D0E2FF" d="M49.5,-16.3C57.5,8.5,52.7,37.2,38.6,45.6C24.5,54,1.2,42.2,-21.7,26.4C-44.5,10.6,-67,-9.3,-63.3,-28.1C-59.6,-46.9,-29.8,-64.7,-4.5,-63.2C20.8,-61.7,41.5,-41,49.5,-16.3Z" transform="translate(100 100)" />
                     </svg> */}
-                    <img src = "images/humanPhoto.png" alt = "profile"/>
+                    {/* <img src = "images/humanPhoto.png" alt = "profile"/> */}
                 </div>
             </div>
         </div>
